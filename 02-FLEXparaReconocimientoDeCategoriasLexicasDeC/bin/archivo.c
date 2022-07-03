385c0ced48a1b102fef67628c69d5153285ae0fd
fernandodanielmaqueda <fmaqueda@frba.utn.edu.ar>
2022-07-02 21:13:52 -0300

    Reglas mas simples pero con problemas en el contexto posterior las constantes reales

---
+++ 02-FLEXparaReconocimientoDeCategoriasLexicasDeC/bin/archivo.c
@@ -0,0 +1,1 @@
+0xFZ// Esto es un test
---
+++ 02-FLEXparaReconocimientoDeCategoriasLexicasDeC/src/funciones.h
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void inicializarString(char** destino, char* fuente)
+{
+    *destino = (char*) malloc((strlen(fuente) + 1) * sizeof(char));
+    if(*destino == NULL)
+    {
+        printf("Hubo un fallo de asignacion de memoria dinamica\n");
+        system("pause");
+        exit(1);
+    }
+    strcpy(*destino, fuente);
+}
+
+void concatenarString(char** destino, char* fuente)
+{
+    *destino = (char*) realloc(*destino, (strlen(*destino) + strlen(fuente) + 1) * sizeof(char));
+    if(*destino == NULL)
+    {
+        printf("Hubo un fallo de asignacion de memoria dinamica\n");
+        system("pause");
+        exit(1);
+    }
+    strcat(*destino, fuente);
+}
+
+/*
+Constantes enteras decimales
+1
+1u
+1l
+1ul
+1lu
+12
+12u
+12l
+12ul
+12lu
+
+Constantes enteras octales
+0
+0u
+0l
+0ul
+0lu
+02
+02u
+02l
+02ul
+02lu
+
+Constantes enteras hexadecimales
+0XF
+0XFu
+0XFl
+0XFul
+0XFlu
+
+Constantes reales
+    Constante Fracción
+.1
+.12
+1.1
+1.12
+12.1
+12.12
+1.
+12.
+
+    Constante Fracción + Parte Exponente
+
+.1e1
+.12e1
+1.1e1
+1.12e1
+12.1e1
+12.12e1
+1.e1
+12.e1
+
+.1e12
+.12e12
+1.1e12
+1.12e12
+12.1e12
+12.12e12
+1.e12
+12.e12
+
+.1e-1
+.12e-1
+1.1e-1
+1.12e-1
+12.1e-1
+12.12e-1
+1.e-1
+12.e-1
+
+.1e-12
+.12e-12
+1.1e-12
+1.12e-12
+12.1e-12
+12.12e-12
+1.e-12
+12.e-12
+
+    Constante Fracción + Sufijo Real
+
+.1f
+.12f
+1.1f
+1.12f
+12.1f
+12.12f
+1.f
+12.f
+
+    Constante Fracción + Parte Exponente + Sufijo Real
+
+.1e1f
+.12e1f
+1.1e1f
+1.12e1f
+12.1e1f
+12.12e1f
+1.e1f
+12.e1f
+
+.1e12f
+.12e12f
+1.1e12f
+1.12e12f
+12.1e12f
+12.12e12f
+1.e12f
+12.e12f
+
+.1e-1f
+.12e-1f
+1.1e-1f
+1.12e-1f
+12.1e-1f
+12.12e-1f
+1.e-1f
+12.e-1f
+
+.1e-12f
+.12e-12f
+1.1e-12f
+1.12e-12f
+12.1e-12f
+12.12e-12f
+1.e-12f
+12.e-12f
+
+    Secuencia dígitos + Parte Exponente
+
+1e1
+12e1
+1e12
+12e12
+1e-1
+12e-1
+1e-12
+12e-12
+
+    Secuencia dígitos + Parte Exponente + Sufijo real
+
+1e1f
+12e1f
+1e12f
+12e12f
+1e-1f
+12e-1f
+1e-12f
+12e-12f
+
+*/
